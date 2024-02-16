e01b1c0ca6c9ebd9df4fbedf9c37369ea3c7ffc8
( •_•)⌐■-■ Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•)⌐■-■ <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-02-16 06:56:39 +0530

    Update fix.js

--- TEST AND DELETE/fix.js
+++ TEST AND DELETE/fix.js
@@ -6,18 +6,6 @@ function addNumbers(num1,num2){
     
 console.log (addnumbers(1,2));    
                  
-           
-       // Make a request to the server to convert the image
-
-
-function convertImageToString() {
-    fetch('/convertImageToString') 
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('imageString').innerText = data; // Display the converted string
-      });    
-
-  }  
         
  
    
