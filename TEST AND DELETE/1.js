8ec08dc44460c1e7861cbfd4bebe8124d9cd4295
( •_•)⌐■-■ Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•)⌐■-■ <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-02-16 07:18:37 +0530

    Update 1.js

---
+++ TEST AND DELETE/1.js
@@ -0,0 +1,12 @@
+
+let a = {
+  name: 'fatfish',
+  toString () {
+    return 'medium'  
+  }  
+}
+
+// My friends, will "hello medium" be printed out?
+if (a == 'medium') {
+  console.log('hello medium')  
+}
