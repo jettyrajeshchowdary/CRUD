8de55263438dcaeb67de58fe92b99940622aa915
( •_•)⌐■-■ Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•)⌐■-■ <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-02-27 18:17:19 +0530

    Update fix.js

--- TEST AND DELETE/fix.js
+++ TEST AND DELETE/fix.js
@@ -6,15 +6,7 @@ function addNumbers(num1,num2){
 }   
             
 console.log (addnumbers(1,2));     
-=======
-                  // fix the error         
-              
-function addNumbers(num1,num2){    
-    return num1 + num2     
-}     
-             
-console.log (addnumbers(1,2));    
-
+               
                    
            
 
